03d9e02bfcf6f22d16690b728cedd26452aafd72
Stefan Weil <sw@weilnetz.de>
2023-11-28 19:27:44 +0100

    Add script which downloads images
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

---
+++ get_images.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+for xml in $(find PPN* -name "*.xml"); do (
+  cd $(dirname $xml)
+  page=$(basename $xml .xml)
+  ppn=$(echo $page|sed "s/_.*//")
+  test -f $page.jpg || wget https://digi.bib.uni-mannheim.de/fileadmin/digi/$ppn/max/$page.jpg
+); done
