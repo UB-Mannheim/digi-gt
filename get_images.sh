658eaa2b7ac323d6200903775fe2f08cc8cb53b0
Stefan Weil <sw@weilnetz.de>
2024-04-19 16:42:03 +0200

    Update image URL (use default instead of max)
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- get_images.sh
+++ get_images.sh
@@ -4,5 +4,5 @@ for xml in $(find PPN* -name "*.xml"); do (
   cd $(dirname $xml)
   page=$(basename $xml .xml)
   ppn=$(echo $page|sed "s/_.*//")
-  test -f $page.jpg || wget https://digi.bib.uni-mannheim.de/fileadmin/digi/$ppn/max/$page.jpg
+  test -f $page.jpg || wget https://digi.bib.uni-mannheim.de/fileadmin/digi/$ppn/default/$page.jpg
 ); done
